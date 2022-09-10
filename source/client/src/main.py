5ec1c0101495744ae3fa751fcb73a7cc08913d2d
Stefan Hemberger <stefan.a.hemberger@gmail.com>
2022-09-10 18:25:49 +0200

    added config

--- source/client/src/main.py
+++ source/client/src/main.py
@@ -1,4 +1,5 @@
 import requests
+import yaml
 
 
 class Client:
@@ -6,26 +7,43 @@ class Client:
         self.uuid = uuid
 
     def start(self):
-        print('Client is alive')
+        print('Client is registered:', self.uuid)
 
 
 def register(api_url) -> str:
+    return 'f24ffee8-9f10-4dda-8832-89c7eeea69a7'
     response = requests.get(api_url).json()
     uuid = response['uuid']
     return uuid
 
 
-def main():
-    registered = False
-    uuid = '57a67049-3e3c-456e-9d7a-f99f88a4c72d'
-    api_url = 'https://apiurl.com/endpoint'
+def create_ini(uuid: str) -> None:
+    with open('config.ini', 'w') as file:
+        file.write(f'clientId: {uuid}')
+        file.close()
 
-    while not registered:
+
+def read_uuid():
+    try:
+        with open('config.ini', 'r') as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+            uuid = config['clientId']
+            return uuid
+    except Exception:
+        return None
+
+
+def main():
+
+    api_url = 'http://192.168.178.27:8080/client/getUUID'
+    uuid = read_uuid()
+
+    while uuid is None:
         user_input = input('Register Client? (y/n): ')
         if user_input in ['y', 'yes', 'j', 'ja']:
             uuid = register(api_url)
             if uuid is not None:
-                registered = True
+                create_ini(uuid)
 
     Client(uuid).start()
 
