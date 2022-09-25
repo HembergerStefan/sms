25583097ba0dd68cc810238b7367c6a87937b56a
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-09-25 20:23:53 +0200

    add search bar

--- source/website/src/components/form/searchbar/SearchBar.tsx
+++ source/website/src/components/form/searchbar/SearchBar.tsx
@@ -1,11 +1,15 @@
 import React, {useEffect, useRef, useState} from 'react';
+import DiceCoefficient from '../../../data/algorithm/DiceCoefficient'
+import TagIcon from '@mui/icons-material/Tag';
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
 import {KeyWords} from '../../../data/searchbar/SearchKeyWords';
 import './SearchBar.css'
 
 const SearchBar = () => {
 
     const [searchText, setSearchText] = useState('')
+    const [data, setData] = useState<string[]>([])
     const formRef = useRef<HTMLFormElement>(null)
 
     useEffect(() => {
@@ -23,31 +27,144 @@ const SearchBar = () => {
     }
 
     const handleSubmit = () => {
-        if (searchText !== "") {
-            KeyWords.map(keyWord => {
-                if (keyWord.name.toLowerCase().includes(searchText.toLowerCase())) {
-                    window.location.replace(keyWord.link);
+        let foundWords: string[] = []
+
+        if (searchText !== '') {
+            KeyWords.forEach(keyWord => {
+                const s = DiceCoefficient.distance(searchText, keyWord.name)
+
+                // Filter if the item should be added or not
+                if (s >= 0.2) {
+                    foundWords.push(keyWord.name)
                 }
             })
+        }
+
+        setData(foundWords)
+    }
+
+    const getLinkToItem = (item: string): string => {
+        let link = '/'
+
+        KeyWords.forEach(keyWord => {
+            if (keyWord.name.toLowerCase().includes(item.toLowerCase())) {
+                link = keyWord.link
+            }
+        })
+
+        return link
+    }
+
+    const lastSearches = (): Array<any> => {
+        let pastSearches: any[] = [];
+
+        if (localStorage['pastSearches']) {
+            pastSearches = JSON.parse(localStorage['pastSearches']);
+        }
+
+        return pastSearches
+    }
+
+    const addLastSearches = (search: string) => {
+        let pastSearches: any[] = lastSearches();
+
+        if (search !== null) {
+            if (pastSearches.indexOf(search) === -1) {
+                pastSearches.unshift(search);
+                if (pastSearches.length > 5) {
+                    pastSearches.pop();
+                }
+
+                localStorage["pastSearches"] = JSON.stringify(pastSearches);
+            }
         }
     }
 
     return (
-        <div id='search-bar--container'>
-            <form ref={formRef} onSubmit={(event) => {
-                event.preventDefault()
-                handleSubmit()
-            }}>
-                <input id='search-bar--text-input' className='fw-regular' type='text'
-                       placeholder='Search by anything'
-                       onChange={nwCt => setSearchText(nwCt.target.value)}
-                       autoComplete='off'/>
+        <>
+            <div id='search-bar--container'>
+                <form ref={formRef} onSubmit={(event) => event.preventDefault()}>
+                    <input id='search-bar--text-input' className='fs-tr-body-1 fw-regular' type='text'
+                           placeholder='Search by anything'
+                           onChange={event => setSearchText(event.target.value)}
+                           onKeyUp={handleSubmit}
+                           autoComplete='off'/>
 
-                <button id='search-btn' type='submit'>
-                    <SearchRoundedIcon style={{color: 'var(--nl-clr-3)'}}/>
-                </button>
-            </form>
-        </div>
+                    <button id='search-btn' type='button'>
+                        <SearchRoundedIcon style={{color: 'var(--nl-clr-3)'}}/>
+                    </button>
+                </form>
+            </div>
+
+            {
+                (data.length > 0) ?
+                    <div id='blocking-container'>
+                        <div id='search-content-container'>
+                            <span className='fs-tr-1 fw--semi-bold'>Search Result</span>
+                            <span className='fs-tr-1 fw--extra-bold' style={{color: 'hsl(342, 98%, 45%)'}}>Insider Feature - Currently Under Development!</span>
+
+                            <div>
+                                <div>
+                                    <span className='fs-pr-body-1 fw--semi-bold stick-to-head'>Recent Searches</span>
+                                    
+                                    {
+                                        (lastSearches().length === 0) ?
+                                            <span className='fs-sc-body-1 fw-regular'>Nothing searched yet ... start searching now!</span> : null
+                                    }
+
+                                    {
+                                        lastSearches().map((cr) => (
+                                            <div className='search-item-container'
+                                                 onClick={() => {
+                                                     window.location.replace(getLinkToItem(cr))
+                                                 }}>
+                                                <div>
+                                                    <TagIcon style={{color: 'hsl(215, 11%, 47%)'}}/>
+                                                </div>
+
+                                                <span className='fs-sc-body-1 fw-regular'>{cr}</span>
+
+                                                <div>
+                                                    <ArrowForwardIosRoundedIcon
+                                                        style={{color: 'hsl(216, 16%, 40%)', fontSize: '15px'}}/>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+
+                                <div>
+                                <span
+                                    className='fs-pr-body-1 fw--semi-bold stick-to-head'>Highest Conformity</span>
+                                    {
+                                        data.map((cr) => (
+                                            <div className='search-item-container'
+                                                 onClick={() => {
+                                                     window.location.replace(getLinkToItem(cr))
+
+                                                     addLastSearches(cr)
+                                                 }}>
+                                                <div>
+                                                    <TagIcon style={{color: 'hsl(215, 11%, 47%)'}}/>
+                                                </div>
+
+                                                <span className='fs-sc-body-1 fw-regular'>{cr}</span>
+
+                                                <div>
+                                                    <ArrowForwardIosRoundedIcon
+                                                        style={{color: 'hsl(216, 16%, 40%)', fontSize: '15px'}}/>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    : null
+            }
+
+        </>
     );
 };
 
