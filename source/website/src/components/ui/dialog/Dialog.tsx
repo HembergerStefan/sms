15e059d7c5dc47a321ecaa5dbabda49f7f463425
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-09-29 08:48:23 +0200

    add shaking effect

--- source/website/src/components/ui/dialog/Dialog.tsx
+++ source/website/src/components/ui/dialog/Dialog.tsx
@@ -7,12 +7,12 @@ interface DialogProps {
     title: string
     unmountComponent: Function
     body: React.ReactNode;
-    footer: React.ReactNode;
+    footer?: React.ReactNode;
 }
 
 const Dialog = ({title, unmountComponent, body, footer}: DialogProps) => {
 
-    const [closeButtonColor, setCloseButtonColor] = useState('')
+    const [shake, setShake] = useState('')
 
     useEffect(() => {
         document.addEventListener('keydown', (event) => {
@@ -23,18 +23,24 @@ const Dialog = ({title, unmountComponent, body, footer}: DialogProps) => {
     }, [])
 
     return (
-        <div className='blocking-container'>
-            <div id='dialog-container'>
+        <div className='blocking-container' onClick={() => {
+            setShake('shake')
+
+            setTimeout(() => {
+                if (shake === '') {
+                    setShake('')
+                }
+            }, 720)
+        }}>
+            <div id='dialog-container' className={shake} onClick={event => event.stopPropagation()}>
                 <span className='fs-tr-1 fw--semi-bold'>{title}</span>
                 <div id='dialog-menu-container'>
-                    <div>
-                        <MoreVertRoundedIcon/>
+                    <div id='more-vert-icon-container'>
+                        <MoreVertRoundedIcon id='more-vert-icon'/>
                     </div>
 
-                    <div onClick={() => unmountComponent()}
-                         onMouseOver={() => setCloseButtonColor('var(--ac-clr-2)')}
-                         onMouseLeave={() => setCloseButtonColor('var(--nl-clr-4)')}>
-                        <CloseRoundedIcon style={{fontSize: '28px', color: closeButtonColor}}/>
+                    <div id='close-icon-container' onClick={() => unmountComponent()}>
+                        <CloseRoundedIcon id='close-icon'/>
                     </div>
                 </div>
 
