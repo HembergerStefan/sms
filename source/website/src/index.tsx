184e57da411de7c56198b49cdc0c7aafd6c2317b
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-09-29 08:49:20 +0200

    add translation for all files

--- source/website/src/index.tsx
+++ source/website/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter} from 'react-router-dom';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import './Translation'; /* Very IMPORTANT import - without it, nothing can be translated */
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
