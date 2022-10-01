40763eb06c44bc9cd894aafe54efa2e586db1775
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-01 20:12:16 +0200

    add font testing

--- source/website/src/pages/Overview.tsx
+++ source/website/src/pages/Overview.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Overview = () => {
     return (
         <div style={{height: '100%', width: '100%', background: 'var(--nl-clr-1)', gridArea: 'main'}}>
-            Main
+            <span className='fs-pr-1 fw--semi-bold clr-pr-1'>This is a heading</span><br/>
+            <span className='fs-pr-body-1 fw-regular clr-sc-1'>This is a Demo Text</span>
         </div>
     );
 };
