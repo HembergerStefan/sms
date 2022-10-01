332b9b37da883ce9c212a4b37f544284797ea2ec
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-01 20:08:14 +0200

    add Tooltip

---
+++ source/website/src/components/ui/tooltip/Tooltip.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useRef} from 'react';
+import useMousePosition from '../../../hooks/useMousePosition';
+import './Tooltip.css'
+
+interface TooltipProps {
+    content: React.ReactNode
+}
+
+const Tooltip = ({content}: TooltipProps) => {
+
+    const mousePosition = useMousePosition()
+
+    const tooltipRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (tooltipRef.current !== null) {
+            let offsetX = tooltipRef.current.offsetWidth / 2
+            let offsetY = tooltipRef.current.offsetHeight + 25
+
+            tooltipRef.current.classList.remove('tooltip-left')
+            tooltipRef.current.classList.remove('tooltip-bottom')
+            tooltipRef.current.classList.add('tooltip-top')
+
+            /* Side Positioning */
+            if (mousePosition.x! < 60) {
+                offsetX = -35
+                offsetY = tooltipRef.current.offsetHeight / 2
+
+                tooltipRef.current.classList.remove('tooltip-top')
+                tooltipRef.current.classList.remove('tooltip-bottom')
+                tooltipRef.current.classList.add('tooltip-left')
+            }
+
+            /* Nav Positioning */
+            if (mousePosition.y! < 60) {
+                offsetX = tooltipRef.current.offsetWidth / 2
+                offsetY = -tooltipRef.current.offsetHeight / 2 + 15
+
+                tooltipRef.current.classList.remove('tooltip-top')
+                tooltipRef.current.classList.remove('tooltip-left')
+                tooltipRef.current.classList.add('tooltip-bottom')
+            }
+
+            tooltipRef.current.setAttribute('style', `top: ${mousePosition.y! - offsetY}px; left: ${mousePosition.x! - offsetX}px`);
+        }
+    }, [mousePosition])
+
+    return (
+        <div ref={tooltipRef} id='tooltip-container'>
+            {content}
+        </div>
+    );
+};
+
+export default Tooltip;
