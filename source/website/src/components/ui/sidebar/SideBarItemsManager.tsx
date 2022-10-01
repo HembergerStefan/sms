637dcdd964473fba79f9b5486a0bdddf340a8721
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-01 20:09:40 +0200

    add SideBarItemsManager which will automatically generate the list item

--- source/website/src/components/ui/sidebar/SideBarItemsManager.tsx
+++ source/website/src/components/ui/sidebar/SideBarItemsManager.tsx
@@ -1,54 +1,19 @@
-import React, {useEffect, useRef} from 'react';
-import {Link, useLocation} from "react-router-dom";
-import DashboardRoundedIcon from "@mui/icons-material/DashboardRounded";
-import DevicesRoundedIcon from "@mui/icons-material/DevicesRounded";
-import GroupsIcon from "@mui/icons-material/Groups";
-import TerminalRoundedIcon from "@mui/icons-material/TerminalRounded";
-import AppsRoundedIcon from "@mui/icons-material/AppsRounded";
-import PersonIcon from "@mui/icons-material/Person";
-import SettingsIcon from "@mui/icons-material/Settings";
+import React from 'react';
 import {NavBarItems} from '../../../data/navbar/NavBarItems';
-import {useTranslation} from 'react-i18next';
-import '../tooltip/Tooltip.css'
+import SideBarListItem from './SideBarListItem';
 
-const SideBarItemsManager = ({startIndex, endIndex}: { startIndex: number, endIndex: number }) => {
+interface SideBarItemsManagerProps {
+    startIndex: number
+    endIndex: number
+}
 
-    const {t} = useTranslation();
-
-    // Assigning location variable
-    const location = useLocation();
-    // Destructuring pathname from location
-    const {pathname} = location;
-    // Javascript split method to get the name of the path in array
-    const splitLocation = pathname.split("/");
-
-    /* List of all the svg mui icon components -> the name is mapped to the original component by reference
-    * by using this the typescript array can use the name of the components and it can be mapped to the real component
-    * by reference */
-    const Components: { [key: string]: any } = {
-        dashboardRoundedIcon: DashboardRoundedIcon,
-        devicesRoundedIcon: DevicesRoundedIcon,
-        groupsIcon: GroupsIcon,
-        terminalRoundedIcon: TerminalRoundedIcon,
-        appsRoundedIcon: AppsRoundedIcon,
-        personIcon: PersonIcon,
-        settingsIcon: SettingsIcon
-    };
+const SideBarItemsManager = ({startIndex, endIndex}: SideBarItemsManagerProps) => {
 
     return (
         <ul>
             {
                 NavBarItems.slice(startIndex, endIndex).map((value, index) => (
-                    <li key={index}
-                        className={`side-image--text-wrapper ${(splitLocation[1] === value.url) ? 'side-active-nav-item' : ''}`}>
-                        <Link to={`/${value.url}`} data-tooltip={t(value.title)}>
-                            {
-                                /* Create the mui svg component */
-                                React.createElement(Components[value.image])
-                            }
-                            <span className='fs-qi-1 fw--semi-bold'>{t(value.title)}</span>
-                        </Link>
-                    </li>
+                    <SideBarListItem key={index} value={value}/>
                 ))
             }
         </ul>
