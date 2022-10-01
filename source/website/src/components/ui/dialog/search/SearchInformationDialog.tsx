9596cf889cdc06815ad38a96f063aa1f7b812eba
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-01 20:12:48 +0200

    add tooltip

--- source/website/src/components/ui/dialog/search/SearchInformationDialog.tsx
+++ source/website/src/components/ui/dialog/search/SearchInformationDialog.tsx
@@ -1,9 +1,11 @@
 import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom';
 import {addLastSearch, getLastSearches} from '../../../../helper/SearchBarHealper';
 import {useTranslation} from 'react-i18next';
 import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
 import SearchBar from '../../../form/searchbar/SearchBar';
-import SearchItemManager from '../../search/search_item/SearchItemManager';
+import SearchResultItemManager from '../../search/search_item/SearchResultItemManager';
+import Tooltip from '../../tooltip/Tooltip';
 import './SearchInformationDialog.css'
 
 const SearchInformationDialog = () => {
@@ -12,6 +14,7 @@ const SearchInformationDialog = () => {
 
     const [searchData, setSearchData] = useState<string[]>([])
     const [lastSearch, setLastSearch] = useState<string[]>([])
+    const [renderComponent, setRenderComponent] = useState(false)
 
     useEffect(() => {
         const deleteIcon = document.querySelector('#delete-icon')
@@ -42,15 +45,17 @@ const SearchInformationDialog = () => {
 
                 {/* Recent searches */}
                 <div className='search-information--result-container'>
-                    <SearchItemManager
+                    <SearchResultItemManager
                         heading={
                             <div id='search-information--recent-search-wrapper' className='stick-to-head'>
-                                <span className='fs-pr-body-1 fw--semi-bold'>{t('Recent Searches')}</span>
+                                <span className='fs-qi-1 fw--semi-bold'>{t('Recent Searches')}</span>
                                 <DeleteRoundedIcon id='delete-icon'
                                                    onClick={() => {
                                                        localStorage.removeItem('lastSearches')
                                                        getLastSearches(setLastSearch)
-                                                   }}/>
+                                                   }}
+                                                   onMouseOver={() => setRenderComponent(true)}
+                                                   onMouseLeave={() => setRenderComponent(false)}/>
                             </div>
                         }
                         data={lastSearch}/>
@@ -58,11 +63,23 @@ const SearchInformationDialog = () => {
 
                 {/* Current search result */}
                 <div className='search-information--result-container'>
-                    <SearchItemManager
-                        heading={<span className='fs-pr-body-1 fw--semi-bold stick-to-head'>{t('Highest Conformity')}</span>}
+                    <SearchResultItemManager
+                        heading={<span
+                            className='fs-qi-1 fw--semi-bold stick-to-head'>{t('Highest Conformity')}</span>}
                         data={searchData} addLastSearch={addSearch}/>
                 </div>
             </div>
+
+            {
+                (renderComponent) ? ReactDOM.createPortal(
+                    <Tooltip content={
+                        <>
+                            <span className='fs-qi-1 fw--semi-bold'>🗑️ {t('Quick Delete')}</span>
+                            <span className='fs-pr-body-1 fw-regular clr-sc-1 mg-t-small'>{t('Click here to delete all the recent searches')}.</span>
+                        </>
+                    }/>,
+                    document.querySelector('#layout-container')!) : null
+            }
         </>
     );
 };
