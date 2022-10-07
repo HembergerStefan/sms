e92bb62557b33a4f031e4726ffa6932a4f8e22a6
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-07 17:26:06 +0200

    change padding

--- source/website/src/pages/Overview.tsx
+++ source/website/src/pages/Overview.tsx
@@ -3,7 +3,14 @@ import Dropdown from "../components/form/dropdown/Dropdown";
 
 const Overview = () => {
     return (
-        <div style={{height: '100%', width: '100%', background: 'var(--nl-clr-1)', padding: '40px', gridArea: 'main'}}>
+        <div style={{
+            height: '100%',
+            width: '100%',
+            background: 'var(--nl-clr-1)',
+            borderRadius: '25px 0 0 0',
+            padding: '40px',
+            gridArea: 'main'
+        }}>
             <span className='fs-pr-1 fw--semi-bold clr-pr-1'>This is a heading</span><br/>
             <span className='fs-pr-body-1 fw-regular clr-sc-1'>This is a Demo Text</span>
 
