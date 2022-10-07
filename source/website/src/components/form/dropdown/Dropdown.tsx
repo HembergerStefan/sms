6043dde81cdf6eb86ec8411068d84dcb0484f9a9
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-07 17:28:15 +0200

    remove deletion functionality

--- source/website/src/components/form/dropdown/Dropdown.tsx
+++ source/website/src/components/form/dropdown/Dropdown.tsx
@@ -1,63 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+import React, {useEffect, useState} from 'react'
+import {useTranslation} from 'react-i18next'
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded'
+import DropdownContent from './DropdownContent'
 import './Dropdown.css'
-import DropdownContent from './DropdownContent';
-import useHover from '../../../hooks/useHover';
-import TooltipManager from "../../ui/tooltip/TooltipManager";
-import {useTranslation} from "react-i18next";
 
 const Dropdown = () => {
 
     const {t} = useTranslation();
 
     const [activeDropdown, setActiveDropdown] = useState<boolean>(false)
-    const [tooltipDropdownText, setTooltipDropdownText] = useState<string>('Click to expand')
-    const [hoverRef, isHovered] = useHover();
-
-    useEffect(() => {
-        window.addEventListener('click', (ev) => {
-
-            const expandDropdownContainer = document.querySelector<Element>('#dropdown-header-container > div')
-            const expandDropdownSVG = document.querySelector<Element>('#dropdown-header-container > div > svg')
-            const expandDropdownPath = document.querySelector<Element>('#dropdown-header-container > div > svg > path')
-
-            if (ev.target !== expandDropdownContainer && ev.target !== expandDropdownSVG && ev.target !== expandDropdownPath) {
-                setActiveDropdown(false)
-            }
-        })
-    }, [])
+    const [selectedItem, setSelectedItem] = useState<string>('Select Item')
 
     useEffect(() => {
         const expandMoreContainer = document.querySelector<Element>('#dropdown-header-container > div')
 
         if (expandMoreContainer !== null) {
             expandMoreContainer.classList.toggle('active-expand-more-icon')
-            if (activeDropdown) {
-                setTooltipDropdownText('Click to collapse')
-            } else {
-                setTooltipDropdownText('Click to expand')
-            }
         }
     }, [activeDropdown])
 
-    return (
-        <>
-            <div id='dropdown-container'>
-                <div id='dropdown-header-container'>
-                    <span className='fs-pr-body-1 fw-regular'>Select Item</span>
-                    <div ref={hoverRef} onClick={() => setActiveDropdown(!activeDropdown)}>
-                        <ExpandMoreRoundedIcon/>
-                    </div>
-                </div>
+    /* Callback function for the dropdown content - so that
+    the selectedItem can be accessed in this component */
+    const setCrItem = (item: string): void => {
+        setSelectedItem(item)
+    }
 
-                <DropdownContent mount={activeDropdown}/>
+    return (
+        <div id='dropdown-container'>
+            <div id='dropdown-header-container' onClick={() => setActiveDropdown(!activeDropdown)}>
+                <span className='fs-pr-body-1 fw-regular'>{t(selectedItem)}</span>
+                <div>
+                    <ExpandMoreRoundedIcon/>
+                </div>
             </div>
 
-            {
-                (isHovered) ? <TooltipManager
-                    content={<span className='fs-pr-body-1 fw--semi-bold'>{t(tooltipDropdownText)}</span>}/> : null
-            }
-        </>
+            <DropdownContent mount={activeDropdown} crItem={setCrItem}/>
+        </div>
     );
 };
 
