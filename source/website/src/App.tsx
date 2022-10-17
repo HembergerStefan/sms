9910e04b69a7584a1b8036719fe18f26c8e428cd
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-17 21:17:59 +0200

    add theming prototype

--- source/website/src/App.tsx
+++ source/website/src/App.tsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import NavBar from "./components/ui/navbar/NavBar";
-import SideBar from "./components/ui/sidebar/SideBar";
-import Overview from "./pages/Overview";
-import Devices from "./pages/Devices";
-import Groups from "./pages/Groups";
-import Scripts from "./pages/Scripts";
-import Packages from "./pages/Packages";
-import User from "./pages/User";
-import Profile from "./pages/Profile";
-import Settings from "./pages/Settings";
-import {Routes, Route} from "react-router-dom";
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+
+import Overview from './pages/Overview'
+import Devices from './pages/Devices'
+import Groups from './pages/Groups'
+import Scripts from './pages/Scripts'
+import Packages from './pages/Packages'
+import User from './pages/User'
+import Profile from './pages/Profile'
+import Settings from './pages/Settings'
+
+import NavBar from './components/ui/navbar/NavBar'
+import SideBar from './components/ui/sidebar/SideBar'
 
 function App() {
+
     return (
-        <div id='layout-container'>
+        <section style={{backgroundColor: 'var(--nl-clr-2)'}} data-theme='moderna-light'>
+            <div id='layout-container'>
 
-            <NavBar/>
-            <SideBar/>
+                <NavBar/>
+                <SideBar/>
 
-            <Routes>
-                <Route path='/' element={<Overview/>}/>
-                <Route path='devices' element={<Devices/>}/>
-                <Route path='groups' element={<Groups/>}/>
-                <Route path='scripts' element={<Scripts/>}/>
-                <Route path='packages' element={<Packages/>}/>
-                <Route path='user' element={<User/>}/>
-                <Route path='profile' element={<Profile/>}/>
-                <Route path='settings' element={<Settings/>}/>
-                <Route path='*' element={<Overview/>}/>
-            </Routes>
-        </div>
-    );
+                <Routes>
+                    <Route path='/' element={<Overview/>}/>
+                    <Route path='devices' element={<Devices/>}/>
+                    <Route path='groups' element={<Groups/>}/>
+                    <Route path='scripts' element={<Scripts/>}/>
+                    <Route path='packages' element={<Packages/>}/>
+                    <Route path='user' element={<User/>}/>
+                    <Route path='profile' element={<Profile/>}/>
+                    <Route path='settings' element={<Settings/>}/>
+                    <Route path='*' element={<Overview/>}/>
+                </Routes>
+            </div>
+        </section>
+    )
 }
 
-export default App;
+export default App
