f82289c504f4edb9eeff8e22cc16536156fcc035
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-10-17 21:21:10 +0200

    cleanup code

--- source/website/src/components/ui/kpi_component/KPIComponent.tsx
+++ source/website/src/components/ui/kpi_component/KPIComponent.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import './KPIComponent.css'
 
 interface KpiComponentProps {
@@ -9,14 +10,15 @@ interface KpiComponentProps {
 }
 
 const KpiComponent = ({title, value, icon, theme}: KpiComponentProps) => {
+    
     return (
         <div id='kpi-container'>
             <div style={{backgroundColor: theme}}>
                 {icon}
             </div>
             <div>
-                <span className='fs-pr-body-1 fw--semi-bold'>{title}</span>
-                <span className='fs-pr-1 fw--extra-bold'>{value}</span>
+                <span className='fs-pr-body-1 fw--semi-bold clr-pr-1'>{title}</span>
+                <span className='fs-pr-1 fw--extra-bold clr-pr-1'>{value}</span>
             </div>
         </div>
     )
