d57c152ff812fcd1dbeefd27e139acf09f91d3d1
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-11-01 08:56:16 +0100

    optimize routing
    add lazy load for components

--- source/website/src/App.tsx
+++ source/website/src/App.tsx
@@ -1,38 +1,40 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import {Routes, Route} from 'react-router-dom'
-
-import Overview from './pages/Overview'
-import Devices from './pages/Devices'
-import Groups from './pages/Groups'
-import Scripts from './pages/Scripts'
-import Packages from './pages/Packages'
-import User from './pages/User'
-import Profile from './pages/Profile'
-import Settings from './pages/Settings'
 
 import NavBar from './components/ui/navbar/NavBar'
 import SideBar from './components/ui/sidebar/SideBar'
 
 function App() {
 
+    const Overview = lazy(() => import('./pages/Overview'));
+    const Devices = lazy(() => import('./pages/Devices'));
+    const Groups = lazy(() => import('./pages/Groups'));
+    const Scripts = lazy(() => import('./pages/Scripts'));
+    const Packages = lazy(() => import('./pages/Packages'));
+    const User = lazy(() => import('./pages/User'));
+    const Profile = lazy(() => import('./pages/Profile'));
+    const Settings = lazy(() => import('./pages/Settings'));
+
     return (
-        <section style={{backgroundColor: 'var(--nl-clr-2)'}} data-theme='moderna-light'>
+        <section style={{backgroundColor: 'var(--nl-clr-2)'}} data-theme='moderna-'>
             <div id='layout-container'>
 
                 <NavBar/>
                 <SideBar/>
 
-                <Routes>
-                    <Route path='/' element={<Overview/>}/>
-                    <Route path='devices' element={<Devices/>}/>
-                    <Route path='groups' element={<Groups/>}/>
-                    <Route path='scripts' element={<Scripts/>}/>
-                    <Route path='packages' element={<Packages/>}/>
-                    <Route path='user' element={<User/>}/>
-                    <Route path='profile' element={<Profile/>}/>
-                    <Route path='settings' element={<Settings/>}/>
-                    <Route path='*' element={<Overview/>}/>
-                </Routes>
+                <Suspense fallback={<></>}>
+                    <Routes>
+                        <Route path='/' element={<Overview/>}/>
+                        <Route path='devices' element={<Devices/>}/>
+                        <Route path='groups' element={<Groups/>}/>
+                        <Route path='scripts' element={<Scripts/>}/>
+                        <Route path='packages' element={<Packages/>}/>
+                        <Route path='user' element={<User/>}/>
+                        <Route path='profile' element={<Profile/>}/>
+                        <Route path='settings' element={<Settings/>}/>
+                        <Route path='*' element={<Overview/>}/>
+                    </Routes>
+                </Suspense>
             </div>
         </section>
     )
