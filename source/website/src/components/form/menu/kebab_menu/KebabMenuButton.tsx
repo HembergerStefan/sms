c8628db16ac37b0c30a55a126ed513c806a0ef07
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-11-12 09:54:21 +0100

    code cleanup for the tooltip

--- source/website/src/components/form/menu/kebab_menu/KebabMenuButton.tsx
+++ source/website/src/components/form/menu/kebab_menu/KebabMenuButton.tsx
@@ -1,6 +1,12 @@
 import React, {memo} from 'react'
 
+import {useTranslation} from 'react-i18next'
+
 import MoreVertRoundedIcon from '@mui/icons-material/MoreVertRounded'
+
+import useHover from '../../../../hooks/useHover'
+
+import TooltipManager from '../../../ui/tooltip/TooltipManager'
 
 import './KebabMenuButton.css'
 
@@ -10,10 +16,21 @@ interface KebabMenuProps {
 
 const KebabMenuButton = ({size}: KebabMenuProps) => {
 
+    const {t} = useTranslation()
+
+    const [hoverRef, isHovered] = useHover()
+
     return (
-        <div id='kebab-menu--container' className='clr-pr-1 md-menu' onClick={() => alert('menu opened')}>
-            <MoreVertRoundedIcon style={{fontSize: size}}/>
-        </div>
+        <>
+            <div ref={hoverRef} id='kebab-menu--container' className='clr-pr-1 md-menu' onClick={() => alert('menu opened')}>
+                <MoreVertRoundedIcon style={{fontSize: size}}/>
+            </div>
+
+            {
+                (isHovered) ? <TooltipManager
+                    content={<span>{t('Settings')}</span>}/> : null
+            }
+        </>
     )
 }
 
