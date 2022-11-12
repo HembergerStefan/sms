bfb5b83468fcba3111208de7dc4e113cfbf61b30
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-11-12 09:51:57 +0100

    add text list item   - display what client executed what script/installed what package

---
+++ source/website/src/components/ui/text_list_component/text_list_item/TextListItem.tsx
@@ -0,0 +1,34 @@
+import React, {memo} from 'react'
+
+import Checkbox from '../../../form/checkbox/Checkbox'
+
+import './TextListItem.css'
+
+interface TextListItemProps {
+    client: string
+    action: string
+    actionDesc: string
+    date: string
+    timeAgo: string
+}
+
+const TextListItem = ({client, action, actionDesc, date, timeAgo}: TextListItemProps) => {
+
+    return (
+        <div id='list-item'>
+            <Checkbox value='checked' handleChange={() => null}/>
+            <div id='info-date--wrapper'>
+                <span className='fs-sc-body-1 fw--semi-bold clr-pr-1'>{client}
+                    <span className='fs-sc-body-1 fw-regular'> {actionDesc} "
+                        <span className='fs-sc-body-1 fw--semi-bold clr-pr-1'>{action}
+                            <span className='fs-sc-body-1 fw-regular'>" on {date}</span>
+                        </span>
+                    </span>
+                </span>
+                <span className='fs-qr-body-1 fw-light'>{timeAgo} ago</span>
+            </div>
+        </div>
+    )
+}
+
+export default memo(TextListItem)
