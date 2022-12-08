1c63b88695d23a435f2c1e99201b5cf875cc2e8b
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-12-08 14:47:00 +0100

    migrate for Zustand script saving/displaying

--- source/website/src/components/ui/dialog/script/ScriptInformationDialog.tsx
+++ source/website/src/components/ui/dialog/script/ScriptInformationDialog.tsx
@@ -1,27 +1,80 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+
+import useScriptStore, {Script, initialState} from '../../../../store/scriptInformationStore'
 
 import TitleInputWrapper from '../../title_input_wrapper/TitleInputWrapper'
 import TextInput from '../../../form/text_input/TextInput'
 import CodeBlock from '../../../form/code_editor/CodeBlockEditor'
+import DateTimePicker from '../../../form/date_time_picker/DateTimePicker'
 
 import './ScriptInformationDialog.css'
 
-const ScriptInformationDialog = () => {
+
+interface ScriptInformationDialogProps {
+    id?: number
+    editMode?: boolean
+}
+
+const ScriptInformationDialog = ({id, editMode = false}: ScriptInformationDialogProps) => {
+
+    /* Get the selected scripts out of the store & the possibility to update the store */
+    const {addingScript, getScriptById} = useScriptStore()
+
+    let selectedScript: Script = initialState
+
+    if (id != null && editMode) {
+        selectedScript = getScriptById(id)
+    }
+
+    useEffect(() => {
+        addingScript.id = 10
+    }, [])
+
+    const setTitle = (content: string) => {
+        addingScript.title = content
+    }
+
+    const setDesc = (content: string) => {
+        addingScript.description = content
+    }
+
+    const setExecDate = (content: Date) => {
+        addingScript.executionDate = content
+    }
+
+    const setCode = (content: string) => {
+        addingScript.code = content
+    }
 
     return (
         <article id='script-information-dialog--container'>
-            <TitleInputWrapper title='Title'
-                               content={<TextInput isHeading={true} placeholder='Script Clarification'/>}/>
-            <TitleInputWrapper title='Description' content={<TextInput placeholder='What makes my script so special ...'
-                                                                       customSize={{
-                                                                           height: 120,
-                                                                           hUnit: 'px',
-                                                                           width: 100,
-                                                                           wUnit: '%'
-                                                                       }}/>}/>
-            <TitleInputWrapper title='Code' content={
-                <CodeBlock/>
-            }/>
+            <section id='title-execution--wrapper'>
+                <TitleInputWrapper title='Title'
+                                   content={<TextInput isHeading={true}
+                                                       defaultValue={editMode ? selectedScript.title : undefined}
+                                                       placeholder='Script Clarification'
+                                                       setStoreValue={setTitle}/>}/>
+                <TitleInputWrapper title='Execution Date' content={
+                    <DateTimePicker defaultValue={editMode ? selectedScript.executionDate : undefined}
+                                    setStoreValue={setExecDate}/>}/>
+            </section>
+
+            <TitleInputWrapper title='Description' content={
+                <TextInput
+                    defaultValue={editMode ? selectedScript.description : undefined}
+                    placeholder='What makes my script so special ...'
+                    customSize={{
+                        height: 120,
+                        hUnit: 'px',
+                        width: 100,
+                        wUnit: '%'
+                    }}
+                    setStoreValue={setDesc}/>}/>
+            <TitleInputWrapper title='Code'
+                               content={<CodeBlock defaultValues={editMode ? {
+                                   code: selectedScript.code,
+                                   language: selectedScript.language
+                               } : undefined} setStoreValue={setCode}/>}/>
         </article>
     )
 }
