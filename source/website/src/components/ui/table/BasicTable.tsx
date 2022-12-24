2e02a431b1132d8c9abe2051ad70fb802bdfa6b4
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-12-24 15:41:12 +0100

    implement basic displaying of BasicTable (version 2)

--- source/website/src/components/ui/table/BasicTable.tsx
+++ source/website/src/components/ui/table/BasicTable.tsx
@@ -1,14 +1,37 @@
-import React, {HTMLProps, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import {useTranslation} from 'react-i18next'
-import {CellContext, createColumnHelper, flexRender, getCoreRowModel, useReactTable,} from '@tanstack/react-table'
+
+import UnfoldMoreRoundedIcon from '@mui/icons-material/UnfoldMoreRounded'
+import KeyboardArrowUpRoundedIcon from '@mui/icons-material/KeyboardArrowUpRounded'
+import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded'
+
+import {
+    CellContext,
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    getSortedRowModel,
+    getPaginationRowModel,
+    SortingState,
+    useReactTable,
+} from '@tanstack/react-table'
 import './BasicTable.css'
-import BoxHeading from "../box_heading_container/BoxHeading";
-import Checkbox from "../../form/checkbox/Checkbox";
+import BoxHeading from '../box_heading_container/BoxHeading'
+import Checkbox from '../../form/checkbox/Checkbox'
+import Numbering from "../numbering/Numbering";
+import useDataListStore from "../../../store/dataListStore";
+import BasicTableDropdownContent from "../../form/dropdown/basic_table/BasicTableDropdownContent";
+import Dropdown from "../../form/dropdown/Dropdown";
 
 const BasicTable = () => {
 
     const {t} = useTranslation()
+
+    const {setTable, getPageIndex, getPageCount} = useDataListStore()
+
+    const [sorting, setSorting] = useState<SortingState>([])
+    const [mountDropdown, setMountDropdown] = useState<boolean>(false)
 
     type Script = {
         id: number
@@ -39,6 +62,34 @@ const BasicTable = () => {
             description: 'Disable Windows Watermark',
             language: 'Python',
             executionDate: '3rd August 2022'
+        },
+        {
+            id: 4,
+            title: 'DWW Test',
+            description: 'Disable Windows Watermark Testing tool',
+            language: 'Python',
+            executionDate: '3rd August 2022'
+        },
+        {
+            id: 5,
+            title: 'Remove DWW',
+            description: 'Disable Windows Watermark deinstaller',
+            language: 'Python',
+            executionDate: '3rd August 2022'
+        },
+        {
+            id: 6,
+            title: 'DWW 2.0',
+            description: 'Disable Windows Watermark in Version 2',
+            language: 'Python',
+            executionDate: '3rd August 2022'
+        },
+        {
+            id: 7,
+            title: 'Cleanup Code',
+            description: 'Script for clean upping code',
+            language: 'Python',
+            executionDate: '3rd August 2022'
         }
     ]
 
@@ -48,29 +99,28 @@ const BasicTable = () => {
         {
             id: 'select',
             header: () => (
-                <IndeterminateCheckbox
-                    {...{
-                        checked: table.getIsAllRowsSelected(),
-                        indeterminate: table.getIsSomeRowsSelected(),
-                        onChange: table.getToggleAllRowsSelectedHandler(),
-                    }}
+                <Checkbox {...{
+                    checked: table.getIsAllRowsSelected(),
+                    indeterminate: table.getIsSomeRowsSelected(),
+                    onChange: table.getToggleAllRowsSelectedHandler()
+                }}
                 />
             ),
             cell: ({row}: CellContext<Script, string>) => (
-                <div className="px-1">
-                    <IndeterminateCheckbox
-                        {...{
-                            checked: row.getIsSelected(),
-                            indeterminate: row.getIsSomeSelected(),
-                            onChange: row.getToggleSelectedHandler(),
-                        }}
-                    />
-                </div>
+                <Checkbox {...{
+                    checked: row.getIsSelected(),
+                    onChange: row.getToggleSelectedHandler(),
+                }}
+                />
             ),
+            enableSorting: false,
         },
         columnHelper.accessor('title', {
             header: () => <h1>Title</h1>,
-            cell: info => <span className='fw--semi-bold clr-pr-1'>{info.getValue()}</span>,
+            cell: info => <div style={{display: 'flex', alignItems: 'center', gap: '16px'}}>
+                <Numbering value={info.row.index}/>
+                <span className='fw--semi-bold clr-pr-1'>{info.getValue()}</span>
+            </div>,
         }),
         columnHelper.accessor('description', {
             header: () => <h1>Description</h1>,
@@ -85,39 +135,22 @@ const BasicTable = () => {
             cell: info => <span>{info.getValue()}</span>,
         })
     ]
-
-    const [rowSelection, setRowSelection] = useState({})
     const [data, setData] = useState(() => [...defaultData])
     const table = useReactTable({
         data,
         columns,
+        state: {
+            sorting
+        },
+        onSortingChange: setSorting,
         getCoreRowModel: getCoreRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getPaginationRowModel: getPaginationRowModel()
     })
 
-
-    function IndeterminateCheckbox({
-                                       indeterminate,
-                                       className = '',
-                                       ...rest
-                                   }: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
-        const ref = React.useRef<HTMLInputElement>(null!)
-
-        React.useEffect(() => {
-            if (typeof indeterminate === 'boolean') {
-                ref.current.indeterminate = !rest.checked && indeterminate
-            }
-        }, [ref, indeterminate])
-
-        return (
-            <input
-                type="checkbox"
-                ref={ref}
-                className={className + ' cursor-pointer'}
-                {...rest}
-            />
-        )
-    }
-
+    useEffect(() => {
+        setTable(table)
+    }, [])
 
     return (
         <>
@@ -130,13 +163,28 @@ const BasicTable = () => {
                             {
                                 headerGroup.headers.map(header => (
                                     <th key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )
-                                        }
+                                        {
+                                            (header.isPlaceholder || header.index === 0) ?
+                                                flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )
+                                                : (
+                                                    <div className={header.index !== 0 ? 'header--content-container' : ''}
+                                                         onClick={header.column.getToggleSortingHandler()}>
+                                                        {flexRender(
+                                                            header.column.columnDef.header,
+                                                            header.getContext()
+                                                        )}
+                                                        {{
+                                                                asc: <KeyboardArrowUpRoundedIcon/>,
+                                                                desc: <KeyboardArrowDownRoundedIcon/>,
+                                                            }
+                                                                [header.column.getIsSorted() as string] ??
+                                                            <UnfoldMoreRoundedIcon/>
+                                                        }
+                                                    </div>
+                                                )}
                                     </th>
                                 ))
                             }
@@ -146,7 +194,7 @@ const BasicTable = () => {
                 </thead>
 
                 {/* For the spacing between the thead and the tbody */}
-                <div></div>
+                <tr style={{height: '10px'}}></tr>
 
                 <tbody id='basic-table--body'>
 
@@ -155,7 +203,10 @@ const BasicTable = () => {
                     heading on the left outer side */}
                 <tr>
                     <td colSpan={columns.length} style={{borderRadius: 'var(--br-r-medium) var(--br-r-medium) 0 0'}}>
-                        <BoxHeading content={<h2 className='fs-qr-1 fw--semi-bold'>{t('All Scripts')}</h2>}/>
+                        <BoxHeading content={<h2 className='fs-qr-1 fw--semi-bold'>{t('All Scripts')}</h2>}
+                                    dropdownContent={<BasicTableDropdownContent setMount={setMountDropdown}/>}
+                                    mountDropdown={mountDropdown}
+                                    setMountDropdown={setMountDropdown}/>
                     </td>
                 </tr>
 
@@ -176,6 +227,14 @@ const BasicTable = () => {
                     ))
                 }
                 </tbody>
+
+                <tfoot id='basic-table--footer'>
+                <tr>
+                    <td colSpan={columns.length}>
+                        <span>{getPageIndex() + 1} of {getPageCount()}</span>
+                    </td>
+                </tr>
+                </tfoot>
             </table>
         </>
     )
