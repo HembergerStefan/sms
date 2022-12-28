f15c10ca80a75fc1da4cb48f2249e7fe16d8ffa4
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-12-28 12:05:48 +0100

    add script table dropdown content

--- source/website/src/components/form/dropdown/basic_table/basic_script_table/BasicScriptTableDropdownContent.tsx
+++ source/website/src/components/form/dropdown/basic_table/basic_script_table/BasicScriptTableDropdownContent.tsx
@@ -2,45 +2,52 @@ import React from 'react'
 
 import {useTranslation} from 'react-i18next'
 
-import useDataListStore from '../../../../store/dataListStore'
+import useDataListScriptStore from '../../../../../store/dataListScriptStore'
 
-import Dropdown from '../Dropdown'
+import Dropdown from '../../Dropdown'
 
-import './BasicTableDropdownContent.css'
+import '../BasicTableDropdownContent.css'
 
-const BasicTableDropdownContent = () => {
+const BasicScriptTableDropdownContent = () => {
 
     const {t} = useTranslation()
 
-    const {nextPage, previousPage, canNextPage, canPreviousPage, setPageSize, pageSize} = useDataListStore()
+    const {
+        setScriptPageSize,
+        scriptPageSize,
+        nextScriptPage,
+        canNextScriptPage,
+        previousScriptPage,
+        canPreviousScriptPage
+    } = useDataListScriptStore()
 
     const PAGE_SIZE_ITEMS: string[] = ['5', '10', '20', '30', '40', '50']
 
     /* User selected item, so change the table size to it */
     const handleChange = (cr: number): void => {
-        setPageSize(cr)
+        setScriptPageSize(cr)
     }
 
     return (
         <ul id='dropdown-content' className='bc-tbl--ddn-cnt--cr'>
             <span className='fs-sc-body-1' style={{marginBottom: '2px'}}>{t('Page Settings')}</span>
             <li>
-                <button onClick={() => previousPage()}
-                        disabled={!canPreviousPage()}>
+                <button onClick={() => previousScriptPage()}
+                        disabled={!canPreviousScriptPage()}>
                     <span>{t('Previous Page')}</span>
                 </button>
             </li>
 
             <li>
-                <button onClick={() => nextPage()}
-                        disabled={!canNextPage()}>
+                <button onClick={() => nextScriptPage()}
+                        disabled={!canNextScriptPage()}>
                     <span>{t('Next Page')}</span>
                 </button>
             </li>
 
             <div>
                 <hr/>
-                <Dropdown prefix='Page Size: ' defaultValue='10' firstSelectedValue={String(pageSize)}
+                <Dropdown prefix='Page Size: ' defaultValue='10' firstSelectedValue={String(scriptPageSize)}
                           items={PAGE_SIZE_ITEMS}
                           handleChange={handleChange}/>
             </div>
@@ -48,4 +55,4 @@ const BasicTableDropdownContent = () => {
     )
 }
 
-export default BasicTableDropdownContent
+export default BasicScriptTableDropdownContent
