d91f4a7205b6a863d10dfc3320508165b39caabe
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-12-28 12:07:49 +0100

    add basic table types

---
+++ source/website/src/components/ui/table/basic_table/basic_table_types/BasicTableTypes.ts
@@ -0,0 +1,16 @@
+export type Script = {
+    id: number
+    title: string
+    description: string
+    executionDate: string
+    language: string
+    status: string
+}
+
+export type Package = {
+    id: number
+    title: string
+    version: string
+    installationDate: string
+    status: string
+}
