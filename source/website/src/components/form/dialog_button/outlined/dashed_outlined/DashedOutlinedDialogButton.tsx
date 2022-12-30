82268f329169bcca368ee60396410bbccb33bca0
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-12-30 15:12:45 +0100

    add handle function for on clicking

--- source/website/src/components/form/dialog_button/outlined/dashed_outlined/DashedOutlinedDialogButton.tsx
+++ source/website/src/components/form/dialog_button/outlined/dashed_outlined/DashedOutlinedDialogButton.tsx
@@ -2,22 +2,19 @@ import React, {memo} from 'react'
 
 import {useTranslation} from 'react-i18next'
 
-import useScriptStore from '../../../../../store/scriptInformationStore'
-
 import './DashedOutlinedDialogButton.css'
 
 interface DialogButtonProps {
     placeholder: string
+    onOnClick?: Function
 }
 
-const DashedOutlinedDialogButton = ({placeholder}: DialogButtonProps) => {
+const DashedOutlinedDialogButton = ({placeholder, onOnClick}: DialogButtonProps) => {
 
     const {t} = useTranslation()
 
-    const {removeScript} = useScriptStore()
-
     return (
-        <button id='dashed-outlined' type='button' onClick={() => removeScript(10)}>
+        <button id='dashed-outlined' type='button' onClick={() => onOnClick ? onOnClick() : undefined}>
             <span className='fs-qi-1 fw--semi-bold clr-nl-3'>{t(placeholder)}</span>
         </button>
     )
