9d312f456dfbffce17d532c41fc1ba29e541c35b
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-12-30 15:10:48 +0100

    add displaying of the selected script id

--- source/website/src/components/ui/dialog/script/ScriptInformationDialog.tsx
+++ source/website/src/components/ui/dialog/script/ScriptInformationDialog.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react'
 
-import useScriptStore, {Script, initialState} from '../../../../store/scriptInformationStore'
+import useScriptStore, {initialScriptState} from '../../../../store/scriptInformationStore'
+import {Script} from '../../../../data/data_types'
 
 import TitleInputWrapper from '../../title_input_wrapper/TitleInputWrapper'
 import TextInput from '../../../form/text_input/TextInput'
@@ -8,7 +9,6 @@ import CodeBlock from '../../../form/code_editor/CodeBlockEditor'
 import DateTimePicker from '../../../form/date_time_picker/DateTimePicker'
 
 import './ScriptInformationDialog.css'
-
 
 interface ScriptInformationDialogProps {
     id?: number
@@ -18,20 +18,20 @@ interface ScriptInformationDialogProps {
 const ScriptInformationDialog = ({id, editMode = false}: ScriptInformationDialogProps) => {
 
     /* Get the selected scripts out of the store & the possibility to update the store */
-    const {addingScript, getScriptById} = useScriptStore()
+    const {scripts, addingScript, getScriptById} = useScriptStore()
 
-    let selectedScript: Script = initialState
+    let selectedScript: Script = initialScriptState
 
     if (id != null && editMode) {
         selectedScript = getScriptById(id)
     }
 
     useEffect(() => {
-        addingScript.id = 10
+        addingScript.id = scripts.length + 1
     }, [])
 
     const setTitle = (content: string) => {
-        addingScript.title = content
+        addingScript.name = content
     }
 
     const setDesc = (content: string) => {
@@ -50,10 +50,12 @@ const ScriptInformationDialog = ({id, editMode = false}: ScriptInformationDialog
         <article id='script-information-dialog--container'>
             <section id='title-execution--wrapper'>
                 <TitleInputWrapper title='Title'
-                                   content={<TextInput isHeading={true}
-                                                       defaultValue={editMode ? selectedScript.title : undefined}
-                                                       placeholder='Script Clarification'
-                                                       setStoreValue={setTitle}/>}/>
+                                   content={<TextInput
+                                       headingId={selectedScript.id !== -1 ? selectedScript.id : scripts.length + 1}
+                                       isHeading={true}
+                                       defaultValue={editMode ? selectedScript.name : undefined}
+                                       placeholder='Script Clarification'
+                                       setStoreValue={setTitle}/>}/>
                 <TitleInputWrapper title='Execution Date' content={
                     <DateTimePicker defaultValue={editMode ? selectedScript.executionDate : undefined}
                                     setStoreValue={setExecDate}/>}/>
