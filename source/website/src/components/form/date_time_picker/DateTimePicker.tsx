bf9547aa7755c8cf20665cd7d6a898e4ae9aa02e
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2022-12-30 15:10:08 +0100

    fix time zone offset miscalculation

--- source/website/src/components/form/date_time_picker/DateTimePicker.tsx
+++ source/website/src/components/form/date_time_picker/DateTimePicker.tsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react'
+import React, {memo, useEffect, useState} from 'react'
 
 import './DateTimePicker.css'
 
@@ -9,10 +9,21 @@ interface DateTimePickerProps {
 
 const DateTimePicker = ({defaultValue, setStoreValue}: DateTimePickerProps) => {
 
+    const [localISOTime, setLocalISOTime] = useState<string>()
+
+    useEffect(() => {
+
+        const tzOffset = new Date().getTimezoneOffset() * 60000 // Offset in milliseconds
+
+        if (defaultValue) {
+            setLocalISOTime(() => new Date(defaultValue.getTime() - tzOffset)
+                .toISOString().slice(0, defaultValue.toISOString().length - 8))
+        }
+
+    }, [])
+
     return (
-        <input id='date-time--picker' className='md-input'
-               defaultValue={defaultValue ? defaultValue.toISOString().slice(0, defaultValue.toISOString().length - 8) : undefined}
-               type='datetime-local'
+        <input id='date-time--picker' className='md-input' defaultValue={localISOTime} type='datetime-local'
                onChange={(event) => setStoreValue(new Date(event.target.value))}/>
     )
 }
