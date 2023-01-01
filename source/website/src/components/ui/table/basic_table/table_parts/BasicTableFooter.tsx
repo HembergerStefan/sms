666ca3d7fec9dd4aaa09763f2be4d527f01f31e3
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-01 01:23:16 +0100

    code cleanup

--- source/website/src/components/ui/table/basic_table/table_parts/BasicTableFooter.tsx
+++ source/website/src/components/ui/table/basic_table/table_parts/BasicTableFooter.tsx
@@ -19,8 +19,8 @@ const BasicTableFooter = ({table, columns}: BasicTableFooterProps) => {
                 {
                     table.getPageCount() === 0 ? null :
                         <span className='fs-sc-body-1'>
-                                    {t('Page')} {table.getState().pagination.pageIndex + 1} {t('of')} {table.getPageCount()}
-                                </span>
+                            {t('Page')} {table.getState().pagination.pageIndex + 1} {t('of')} {table.getPageCount()}
+                        </span>
                 }
             </td>
         </tr>
