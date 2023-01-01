35479726c1a87351091f34ffb4d66ca37ab7c1d2
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-01 21:15:57 +0100

    add package remove and add functionality

--- source/website/src/components/form/dropdown/basic_table/basic_packaget_table/BasicPackageTableDropdownContent.tsx
+++ source/website/src/components/form/dropdown/basic_table/basic_packaget_table/BasicPackageTableDropdownContent.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {useTranslation} from 'react-i18next'
 
+import usePackageStore from '../../../../../store/packageInformationStore'
 import useDataListPackageStore from '../../../../../store/dataListPackageStore'
 
+import AppsRoundedIcon from '@mui/icons-material/AppsRounded'
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded'
+
 import Dropdown from '../../Dropdown'
+import DialogManager, {DialogManagerTypes} from '../../../../ui/dialog/DialogManager'
 
 import '../BasicTableDropdownContent.css'
 
-const BasicPackageTableDropdownContent = () => {
+interface BasicPackageTableDropdownContentProps {
+    setMountDropdown: Function
+}
+
+const BasicPackageTableDropdownContent = ({setMountDropdown}: BasicPackageTableDropdownContentProps) => {
 
     const {t} = useTranslation()
 
@@ -18,38 +27,87 @@ const BasicPackageTableDropdownContent = () => {
         nextPackagePage,
         canNextPackagePage,
         previousPackagePage,
-        canPreviousPackagePage
+        canPreviousPackagePage,
+        selectionPackageRows
     } = useDataListPackageStore()
 
-    const PAGE_SIZE_ITEMS: string[] = ['5', '10', '20', '30', '40', '50']
+    const {removePackage} = usePackageStore()
+
+    const [renderDialogComponent, setRenderDialogComponent] = useState<boolean>(false)
+
+    const PAGE_SIZE_ITEMS: number[] = [5, 10, 20, 30, 40, 50]
 
     /* User selected item, so change the table size to it */
     const handleChange = (cr: number): void => {
         setPackagePageSize(cr)
     }
 
+    /* On add open the dialog for adding */
+    const add = () => {
+        setMountDropdown(() => false)
+        setRenderDialogComponent(() => true)
+    }
+
+    /* Remove all selected script entries */
+    const remove = () => {
+        selectionPackageRows.forEach(id => {
+            removePackage(id)
+        })
+    }
+
     return (
-        <ul id='dropdown-content' className='bc-tbl--ddn-cnt--cr'>
-            <li>
-                <button onClick={() => previousPackagePage()}
-                        disabled={!canPreviousPackagePage()}>
-                    <span>{t('Previous Page')}</span>
-                </button>
-            </li>
+        <>
+            <ul id='dropdown-content' className='bc-tbl--ddn-cnt--cr'>
 
-            <li>
-                <button onClick={() => nextPackagePage()}
-                        disabled={!canNextPackagePage()}>
-                    <span>{t('Next Page')}</span>
-                </button>
-            </li>
+                <li>
+                    <button onClick={() => previousPackagePage()}
+                            disabled={!canPreviousPackagePage()}>
+                        <span>{t('Previous Page')}</span>
+                    </button>
+                </li>
 
-            <div>
-                <Dropdown prefix='Page Size: ' defaultValue='10' firstSelectedValue={String(packagePageSize)}
-                          items={PAGE_SIZE_ITEMS}
-                          handleChange={handleChange}/>
-            </div>
-        </ul>
+                <li>
+                    <button onClick={() => nextPackagePage()}
+                            disabled={!canNextPackagePage()}>
+                        <span>{t('Next Page')}</span>
+                    </button>
+                </li>
+
+                <hr/>
+
+                <div>
+                    <Dropdown prefix='Page Size' defaultValue={10} firstSelectedValue={packagePageSize}
+                              items={PAGE_SIZE_ITEMS}
+                              handleChange={handleChange}/>
+                </div>
+
+                <hr/>
+
+                <li>
+                    <button onClick={() => add()}>
+                        <div style={{color: 'var(--sc-clr)'}}>
+                            <AppsRoundedIcon/>
+                            <span>{t('Add')}</span>
+                        </div>
+                    </button>
+                </li>
+
+                <li>
+                    <button onClick={() => remove()}
+                            disabled={selectionPackageRows.length === 0}>
+                        <div style={{color: 'var(--ac-clr-2)'}}>
+                            <DeleteRoundedIcon/>
+                            <span
+                                style={{color: 'var(--ac-clr-2)'}}>{t('Remove')} {selectionPackageRows.length !== 0 ? `(${selectionPackageRows.length})` : ''}</span>
+                        </div>
+                    </button>
+                </li>
+            </ul>
+
+            <DialogManager dialogTyp={DialogManagerTypes.PACKAGE} title='Add Package' editMode={false}
+                           renderComponent={renderDialogComponent}
+                           setRenderComponent={setRenderDialogComponent}/>
+        </>
     )
 }
 
