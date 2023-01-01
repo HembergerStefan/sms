b694882ed2e2a67ef937fe1cb05ae0d6db7b6d96
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-01 01:21:16 +0100

    add features for dropdown

--- source/website/src/components/form/dropdown/basic_table/basic_script_table/BasicScriptTableDropdownContent.tsx
+++ source/website/src/components/form/dropdown/basic_table/basic_script_table/BasicScriptTableDropdownContent.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 import {useTranslation} from 'react-i18next'
 
+import NoteAddRoundedIcon from '@mui/icons-material/NoteAddRounded'
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded'
+
+import useScriptStore from '../../../../../store/scriptInformationStore'
 import useDataListScriptStore from '../../../../../store/dataListScriptStore'
 
 import Dropdown from '../../Dropdown'
+import DialogManager from '../../../../ui/dialog/DialogManager'
 
 import '../BasicTableDropdownContent.css'
 
-const BasicScriptTableDropdownContent = () => {
+interface BasicScriptTableDropdownContentProps {
+    setMountDropdown: Function
+}
+
+const BasicScriptTableDropdownContent = ({setMountDropdown}: BasicScriptTableDropdownContentProps) => {
 
     const {t} = useTranslation()
 
@@ -18,40 +27,86 @@ const BasicScriptTableDropdownContent = () => {
         nextScriptPage,
         canNextScriptPage,
         previousScriptPage,
-        canPreviousScriptPage
+        canPreviousScriptPage,
+        selectionScriptRows,
     } = useDataListScriptStore()
 
-    const PAGE_SIZE_ITEMS: string[] = ['5', '10', '20', '30', '40', '50']
+    const {removeScript} = useScriptStore()
+
+    const [renderDialogComponent, setRenderDialogComponent] = useState<boolean>(false)
+
+    const PAGE_SIZE_ITEMS: number[] = [5, 10, 20, 30, 40, 50]
 
     /* User selected item, so change the table size to it */
     const handleChange = (cr: number): void => {
         setScriptPageSize(cr)
     }
 
+    /* On add open the dialog for adding */
+    const add = () => {
+        setMountDropdown(() => false)
+        setRenderDialogComponent(() => true)
+    }
+
+    /* Remove all selected script entries */
+    const remove = () => {
+        selectionScriptRows.forEach(id => {
+            removeScript(id)
+        })
+    }
+
     return (
-        <ul id='dropdown-content' className='bc-tbl--ddn-cnt--cr'>
-            <span className='fs-sc-body-1' style={{marginBottom: '2px'}}>{t('Page Settings')}</span>
-            <li>
-                <button onClick={() => previousScriptPage()}
-                        disabled={!canPreviousScriptPage()}>
-                    <span>{t('Previous Page')}</span>
-                </button>
-            </li>
+        <>
+            <ul id='dropdown-content' className='bc-tbl--ddn-cnt--cr'>
 
-            <li>
-                <button onClick={() => nextScriptPage()}
-                        disabled={!canNextScriptPage()}>
-                    <span>{t('Next Page')}</span>
-                </button>
-            </li>
+                <li>
+                    <button onClick={() => previousScriptPage()}
+                            disabled={!canPreviousScriptPage()}>
+                        <span>{t('Previous Page')}</span>
+                    </button>
+                </li>
 
-            <div>
+                <li>
+                    <button onClick={() => nextScriptPage()}
+                            disabled={!canNextScriptPage()}>
+                        <span>{t('Next Page')}</span>
+                    </button>
+                </li>
+
                 <hr/>
-                <Dropdown prefix='Page Size: ' defaultValue='10' firstSelectedValue={String(scriptPageSize)}
-                          items={PAGE_SIZE_ITEMS}
-                          handleChange={handleChange}/>
-            </div>
-        </ul>
+
+                <div>
+                    <Dropdown prefix='Page Size' defaultValue={10} firstSelectedValue={scriptPageSize}
+                              items={PAGE_SIZE_ITEMS}
+                              handleChange={handleChange}/>
+                </div>
+
+                <hr/>
+
+                <li>
+                    <button onClick={() => add()}>
+                        <div style={{color: 'var(--sc-clr)'}}>
+                            <NoteAddRoundedIcon/>
+                            <span>{t('Add')}</span>
+                        </div>
+                    </button>
+                </li>
+
+                <li>
+                    <button onClick={() => remove()}
+                            disabled={selectionScriptRows.length === 0}>
+                        <div style={{color: 'var(--ac-clr-2)'}}>
+                            <DeleteRoundedIcon/>
+                            <span
+                                style={{color: 'var(--ac-clr-2)'}}>{t('Remove')} {selectionScriptRows.length !== 0 ? `(${selectionScriptRows.length})` : ''}</span>
+                        </div>
+                    </button>
+                </li>
+            </ul>
+
+            <DialogManager title='Add Script' editMode={false} renderComponent={renderDialogComponent}
+                           setRenderComponent={setRenderDialogComponent}/>
+        </>
     )
 }
 
