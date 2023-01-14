614cf7e4e1dd113f5004607daa4282b13a8e49c8
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-14 10:41:08 +0100

    remove chart demo content prepare for implementation

--- source/website/src/components/ui/chart/ChartContainer.tsx
+++ source/website/src/components/ui/chart/ChartContainer.tsx
@@ -1,69 +1,12 @@
 import React from 'react'
 
 import './ChartContainer.css'
-import {AxisOptions, Chart} from "react-charts";
 
 const ChartContainer = () => {
 
-    type DailyStars = {
-        date: Date,
-        stars: number,
-    }
-
-    type Series = {
-        label: string,
-        data: DailyStars[]
-    }
-
-    const data: Series[] = [
-        {
-            label: 'React Charts',
-            data: [
-                {
-                    date: new Date(),
-                    stars: 24,
-                }
-                // ...
-            ]
-        },
-        {
-            label: 'React Query',
-            data: [
-                {
-                    date: new Date(),
-                    stars: 12,
-                }
-                // ...
-            ]
-        }
-    ]
-
-    const primaryAxis = React.useMemo(
-        (): AxisOptions<DailyStars> => ({
-            getValue: datum => datum.date,
-        }),
-        []
-    )
-
-    const secondaryAxes = React.useMemo(
-        (): AxisOptions<DailyStars>[] => [
-            {
-                getValue: datum => datum.stars,
-            },
-        ],
-        []
-    )
-
     return (
         <article id='charts-container' className='box'>
-                <Chart
-                    options={{
-                        data,
-                        primaryAxis,
-                        secondaryAxes,
 
-                    }}
-                />
         </article>
     )
 }
