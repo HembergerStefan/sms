a07784e38d2fbcc90ff9b3c0a7d76e1d5e2931e0
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-22 14:39:51 +0100

    add online status manager style

---
+++ source/website/src/components/ui/online_status_displaying/OnlineStatus.tsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react'
+
+import {useTranslation} from 'react-i18next'
+
+import useClientStore from '../../../store/clientInformationStore'
+import {Client} from '../../../data/data_types'
+
+import useHover from '../../../hooks/useHover'
+
+import TooltipManager from '../tooltip/TooltipManager'
+
+import './OnlineStatus.css'
+
+interface OnlineStatusProps {
+    client: Client
+}
+
+const OnlineStatus = ({client}: OnlineStatusProps) => {
+
+    const {t} = useTranslation()
+
+    const {getClientOnlineStatus} = useClientStore()
+
+    const [hoverRef, isHovered] = useHover()
+
+    const [status, setStatus] = useState<{ status: 'Online' | 'Offline', lastOnline: string }>({
+        status: 'Offline',
+        lastOnline: ''
+    })
+
+    useEffect(() => {
+        /* Get default data */
+        setStatus(() => getClientOnlineStatus(client.macAddress))
+
+        /* Get new data every minute */
+        setInterval(() => {
+            setStatus(() => getClientOnlineStatus(client.macAddress))
+        }, 60_000)
+    }, [])
+
+    return (
+        status.status.toLowerCase() === 'online' ?
+            <span id='status-online' className='status fs-tr-body-1 fw--semi-bold'>{status.status}</span>
+            :
+            <>
+                <span ref={hoverRef} id='status-offline' className='status fs-tr-body-1 fw--semi-bold'>
+                    {status.status}
+                </span>
+
+                {
+                    (isHovered) ?
+                        <TooltipManager content={
+                            <span className='fs-sc-body-1'>{t('Last online')} {' '}
+                                <span className='fs-sc-body-1 fw--semi-bold'>{t(status.lastOnline)}</span>
+                            </span>
+                        }/> : null
+                }
+            </>
+    )
+}
+
+export default OnlineStatus
