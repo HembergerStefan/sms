22cf842036ce7f3336f1ff7bae29af9a859733e2
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-22 14:40:26 +0100

    add ip input field style

---
+++ source/website/src/components/ui/ip_input/IPInput.tsx
@@ -0,0 +1,37 @@
+import React, {memo, useRef} from 'react'
+
+import ContentCopyRoundedIcon from '@mui/icons-material/ContentCopyRounded'
+
+import './IPInput.css'
+
+interface IpInputProps {
+    value: string
+}
+
+const IpInput = ({value}: IpInputProps) => {
+
+    const ipInputRef = useRef<HTMLDivElement>(null)
+
+    const copyToClipboard = () => {
+        /* TODO: ADD COPY SUCCESS STATE NOTIFICATION IN NOTIFICATION TAB (HEADER) */
+        navigator.clipboard.writeText(value).then(text => {
+            console.log(`copied ${value}`)
+
+            if (ipInputRef.current !== null) {
+                ipInputRef.current.classList.add('success-copied')
+            }
+        })
+            .catch(reason => console.log(reason))
+    }
+
+    return (
+        <div ref={ipInputRef} id='ip-input--container' className='clr-sc-1'>
+            <span className='fs-sc-body-1'>{value}</span>
+            <div onClick={() => copyToClipboard()}>
+                <ContentCopyRoundedIcon style={{fontSize: 'var(--fs-qi-heading)'}}/>
+            </div>
+        </div>
+    )
+}
+
+export default memo(IpInput)
