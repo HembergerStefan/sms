92afce6265d9240d1ec28c7c38c269f7bcadb5b2
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-01-22 14:36:11 +0100

    remove profile page

--- source/website/src/App.tsx
+++ source/website/src/App.tsx
@@ -13,7 +13,6 @@ function App() {
     const Scripts = lazy(() => import('./pages/Scripts'))
     const Packages = lazy(() => import('./pages/Packages'))
     const User = lazy(() => import('./pages/User'))
-    const Profile = lazy(() => import('./pages/Profile'))
     const Settings = lazy(() => import('./pages/Settings'))
 
     useEffect(() => {
@@ -39,7 +38,6 @@ function App() {
                         <Route path='scripts' element={<Scripts/>}/>
                         <Route path='packages' element={<Packages/>}/>
                         <Route path='user' element={<User/>}/>
-                        <Route path='profile' element={<Profile/>}/>
                         <Route path='settings' element={<Settings/>}/>
                         <Route path='*' element={<Overview/>}/>
                     </Routes>
