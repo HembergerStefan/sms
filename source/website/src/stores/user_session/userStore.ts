50b67a30fb4b82882ebe773d9858a7b5204e69cd
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-02-09 23:28:04 +0100

    add user role and id

--- source/website/src/stores/user_session/userStore.ts
+++ source/website/src/stores/user_session/userStore.ts
@@ -2,21 +2,33 @@ import create from 'zustand'
 import {persist} from 'zustand/middleware'
 
 export interface UserStore {
+    id: string
     username: string | undefined
     token: string | undefined
+    roleId: string
+    setUserId: (id: string) => void
     setUsername: (username: string) => void
     setUserToken: (token: string) => void
+    setUserRole: (id: string) => void
 }
 
 const useUserStore = create(
     persist<UserStore>((set, get) => ({
+            id: '',
             username: '',
             token: undefined,
+            roleId: '',
+            setUserId: (id) => set(prev => ({
+                id: prev.id = id
+            })),
             setUsername: (username) => set(prev => ({
                 username: prev.username = username
             })),
             setUserToken: (token) => set(prev => ({
                 token: prev.token = token
+            })),
+            setUserRole: (id) => set(prev => ({
+                roleId: prev.roleId = id
             })),
         }), {
             name: 'user-store',
