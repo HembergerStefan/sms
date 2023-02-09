dce7658ff0f33f0a7d5046c9ec3a4d22469c1fb6
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-02-09 23:36:20 +0100

    add types

--- source/website/src/data/data_types/index.ts
+++ source/website/src/data/data_types/index.ts
@@ -1,10 +1,12 @@
 import {Script} from './script'
 import {Package} from './package'
 import {Client} from './client'
+import {Group} from './group'
+import {Role} from './role'
 import {AvailableClient} from './availableClient'
 
-export type {Script, Package, Client, AvailableClient}
+export type {Script, Package, Client, Group, Role, AvailableClient}
 
 export enum DataTypes {
-    SCRIPT, PACKAGE, CLIENT, GROUPS, USER, AVAILABLE_CLIENT
+    SCRIPT, PACKAGE, CLIENT, GROUP, USER, AVAILABLE_CLIENT
 }
