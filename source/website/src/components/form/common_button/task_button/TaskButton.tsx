cef4d6b5b0d758025456c78a8dac2e0c508b95af
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-03-10 19:28:19 +0100

    code cleanup + translation

--- source/website/src/components/form/common_button/task_button/TaskButton.tsx
+++ source/website/src/components/form/common_button/task_button/TaskButton.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import {useTranslation} from 'react-i18next'
+
 import './TaskButton.css'
 
 interface TaskButtonProps {
     task: Function
     taskName: string
+    icon?: React.ReactNode
 }
 
-const TaskButton = ({task, taskName}: TaskButtonProps) => {
+const TaskButton = ({task, taskName, icon}: TaskButtonProps) => {
+
+    const {t} = useTranslation()
 
     const handleTask = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault()
@@ -16,7 +21,8 @@ const TaskButton = ({task, taskName}: TaskButtonProps) => {
 
     return (
         <button id='task-button' className='md-menu' onClick={handleTask}>
-            <span className='clr-nl-3 fs-sc-body-1 fw--semi-bold'>{taskName}</span>
+            {icon ?? null}
+            <span className='clr-nl-3 fs-sc-body-1 fw--semi-bold'>{t(taskName)}</span>
         </button>
     )
 }
