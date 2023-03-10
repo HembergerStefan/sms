26c94ae6bda4e1cbbbb435522c8c1b964f43b5c8
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-03-10 19:17:12 +0100

    add background

--- source/website/src/pages/landing_page/LandingPage.tsx
+++ source/website/src/pages/landing_page/LandingPage.tsx
@@ -10,6 +10,7 @@ const LandingPage = () => {
             alignItems: 'center',
             height: '100vh',
             width: '100vw',
+            backgroundColor: 'hsl(0, 0%, 100%)'
         }}>
             <Login/>
         </div>
