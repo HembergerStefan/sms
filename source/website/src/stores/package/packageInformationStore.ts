422804bd5e5f6c0f7ac8b73757e166f725945177
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-03-10 19:36:59 +0100

    add helper methods

--- source/website/src/stores/package/packageInformationStore.ts
+++ source/website/src/stores/package/packageInformationStore.ts
@@ -1,4 +1,4 @@
-import create from 'zustand'
+import {create} from 'zustand'
 
 import {Package} from '../../data/data_types'
 
@@ -7,13 +7,14 @@ export interface PackageSlice {
     setPackages: (entries: Package[]) => void
     addingPackage: Package
     addPackage: (entry: Package) => void
-    removePackage: (id: number) => void
+    removePackage: (id: string) => void
     resetPackages: () => void
-    getPackageById: (id: number) => Package
+    resetAddingPackage: () => void
+    getPackageById: (id: string) => Package
 }
 
 export const initialPackageState: Package = {
-    id: -1,
+    id: '',
     name: '',
     version: '',
     url: '',
@@ -22,56 +23,67 @@ export const initialPackageState: Package = {
 }
 
 const usePackageStore = create<PackageSlice>((set, get) => ({
-    _packages: [],
-    addingPackage: {
-        id: -1,
-        name: '',
-        version: '',
-        url: '',
-        addingDate: new Date(),
-        silentSwitch: ''
-    },
-    setPackages: (entries) => set(state => ({
-        _packages: state._packages = entries
-    })),
-    addPackage: (entry) => {
-        let _packages = get()._packages
+        _packages: [],
+        addingPackage: {
+            id: '',
+            name: '',
+            version: '',
+            url: '',
+            addingDate: new Date(),
+            silentSwitch: ''
+        },
+        setPackages: (entries) => set(state => ({
+            _packages: state._packages = entries
+        })),
+        addPackage: (entry) => {
+            let _packages = get()._packages
 
-        _packages.forEach(_package => {
-            if (_package.id === entry.id) {
-                _packages = _packages.filter((_package) => _package.id !== entry.id)
+            _packages.forEach(_package => {
+                if (_package.id === entry.id) {
+                    _packages = _packages.filter((_package) => _package.id !== entry.id)
+                }
+            })
+
+            _packages.push(entry)
+
+            set((state) => ({
+                _packages: state._packages = [..._packages],
+                addingPackage: state.addingPackage = {
+                    id: '',
+                    name: '',
+                    version: '',
+                    url: '',
+                    addingDate: new Date(),
+                    silentSwitch: ''
+                } // Reset, to add new data
+            }))
+        },
+        removePackage: (id) => set((state) => ({
+            _packages: state._packages.filter((_package) => _package.id !== id)
+        })),
+        resetPackages: () => set(state => ({
+            _packages: state._packages = []
+        })),
+        getPackageById: (id) => {
+            const selectedPackage = get()._packages.find((_package) => _package.id === id)
+
+            if (selectedPackage !== undefined) {
+                return selectedPackage
             }
-        })
 
-        _packages.push(entry)
-
-        set((state) => ({
-            _packages: state._packages = [..._packages],
+            return initialPackageState
+        },
+        resetAddingPackage: () => set(state => ({
             addingPackage: state.addingPackage = {
-                id: -1,
+                id: '',
                 name: '',
                 version: '',
                 url: '',
                 addingDate: new Date(),
                 silentSwitch: ''
             } // Reset, to add new data
-        }))
-    },
-    removePackage: (id) => set((state) => ({
-        _packages: state._packages.filter((_package) => _package.id !== id)
-    })),
-    resetPackages: () => set(state => ({
-        _packages: state._packages = []
-    })),
-    getPackageById: (id) => {
-        const selectedPackage = get()._packages.find((_package) => _package.id === id)
-
-        if (selectedPackage !== undefined) {
-            return selectedPackage
-        }
-
-        return initialPackageState
-    }
-}))
+        })),
+    })
+)
 
 export default usePackageStore
