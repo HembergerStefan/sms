071f87287982565826de537611318880f63b3bd9
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-03-11 11:37:54 +0100

    add id-tag

--- source/website/src/pages/Settings.tsx
+++ source/website/src/pages/Settings.tsx
@@ -14,7 +14,7 @@ const Clients = () => {
         <section id='dashboard-layout--container'>
             <h1 className='fs-tr-1 fw--semi-bold'>{t('Settings')}</h1>
 
-            <div style={{display: 'flex', flexDirection: 'column', gap: '25px'}}>
+            <div id='appearance-container' style={{display: 'flex', flexDirection: 'column', gap: '25px'}}>
                 <div>
                     <h2 className='fs-qr-1 fw--semi-bold'>{t('Appearance')}</h2>
                     <span>{t('Change how Systems Management Server looks and feels in your browser')}.</span>
