fa6586ca6abc6aa3b4b5d9625be5d6254a30a882
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-03-11 08:40:53 +0100

    code cleanup add display value for the numbering change selectedId type to string migrate to react query mutations + automatically detect which dialog content should be rendered/modified/deleted add case for user not permitted to modify

--- source/website/src/components/ui/dialog/DialogManager.tsx
+++ source/website/src/components/ui/dialog/DialogManager.tsx
@@ -5,21 +5,38 @@ import {useTranslation} from 'react-i18next'
 
 import {DataTypes} from '../../../data/data_types'
 
-import useScriptStore from '../../../stores/scriptInformationStore'
-import usePackageStore from '../../../stores/packageInformationStore'
+import useScriptStore from '../../../stores/script/scriptInformationStore'
+import usePackageStore from '../../../stores/package/packageInformationStore'
+import useGroupStore from '../../../stores/groupInformationStore'
+import useClientStore from '../../../stores/clientInformationStore'
+import useUserInfoStore from '../../../stores/user/userInformationStore'
+
+import {
+    useRemoveClientMutation,
+    useRemoveGroupMutation,
+    useRemovePackageMutation,
+    useRemoveScriptMutation, useRemoveUserMutation,
+    useUpdateClientMutation,
+    useUpdateGroupMutation,
+    useUpdatePackageMutation,
+    useUpdateScriptMutation, useUpdateUserMutation, useUserPermittedQuery
+} from '../../../utils/api/ApiService'
 
 import Dialog from './Dialog'
 import ScriptInformationDialog from './script/ScriptInformationDialog'
 import PackageInformationDialog from './package/PackageInformationDialog'
+import GroupInformationDialog from './group/GroupInformationDialog'
 import SolidDialogButton from '../../form/dialog_button/solid/SolidDialogButton'
 import DashedOutlinedDialogButton from '../../form/dialog_button/outlined/dashed_outlined/DashedOutlinedDialogButton'
-import SolidOutlinedDialogButton from '../../form/dialog_button/outlined/solid_outlined/SolidOutlinedDialogButton'
+import ClientInformationDialog from './client/ClientInformationDialog'
+import UserInformationDialog from './user/UserInformationDialog'
 
 interface DialogManagerProps {
     dialogTyp: DataTypes
     title: string
     editMode: boolean
-    selectedId?: number
+    selectedId?: string
+    displayId: number
     renderComponent: boolean
     setRenderComponent: Function
 }
@@ -29,33 +46,95 @@ const DialogManager = ({
                            title,
                            editMode,
                            selectedId,
+                           displayId,
                            renderComponent,
                            setRenderComponent
                        }: DialogManagerProps) => {
 
     const {t} = useTranslation()
 
-    const {removeScript} = useScriptStore()
-    const {removePackage} = usePackageStore()
+    const {addingScript} = useScriptStore()
+    const {addingPackage} = usePackageStore()
+    const {addingGroup} = useGroupStore()
+    const {addingClient} = useClientStore()
+    const {addingUser} = useUserInfoStore()
 
     const [dialogContent, setDialogContent] = useState<React.ReactNode>(null)
+    const [userPermitted, setUserPermitted] = useState<boolean>(false)
+
+    const userPermittedQuery = useUserPermittedQuery()
+
+    const scriptUpdateMutation = useUpdateScriptMutation(addingScript)
+    const packageUpdateMutation = useUpdatePackageMutation(addingPackage)
+    const groupUpdateMutation = useUpdateGroupMutation(addingGroup)
+    const clientUpdateMutation = useUpdateClientMutation(addingClient)
+    const userUpdateMutation = useUpdateUserMutation(addingUser)
+
+    const packageRemoveMutation = useRemovePackageMutation()
+    const scriptRemoveMutation = useRemoveScriptMutation()
+    const groupRemoveMutation = useRemoveGroupMutation()
+    const clientRemoveMutation = useRemoveClientMutation()
+    const userRemoveMutation = useRemoveUserMutation()
+
+    useEffect(() => {
+        if (userPermittedQuery.isSuccess && userPermittedQuery.data.name === 'Admin') {
+            setUserPermitted(() => true)
+        }
+    }, [userPermittedQuery.data])
 
     useEffect(() => {
         if (dialogTyp === 0) {
             setDialogContent(() => <ScriptInformationDialog id={selectedId ? selectedId : undefined}
+                                                            displayId={displayId}
                                                             editMode={editMode}/>)
-        } else {
+        } else if (dialogTyp === 1) {
             setDialogContent(() => <PackageInformationDialog id={selectedId ? selectedId : undefined}
+                                                             displayId={displayId}
                                                              editMode={editMode}/>)
+        } else if (dialogTyp === 2) {
+            setDialogContent(() => <ClientInformationDialog id={selectedId ? selectedId : undefined}
+                                                            displayId={displayId}
+                                                            editMode={editMode}/>)
+        } else if (dialogTyp === 3) {
+            setDialogContent(() => <GroupInformationDialog id={selectedId ? selectedId : undefined}
+                                                           displayId={displayId}
+                                                           editMode={editMode}/>)
+        } else if (dialogTyp === 4) {
+            setDialogContent(() => <UserInformationDialog id={selectedId ? selectedId : undefined}
+                                                          editMode={editMode}/>)
         }
     }, [selectedId, dialogTyp])
+
+    const update = () => {
+        if (selectedId) {
+            if (dialogTyp === 0) {
+                scriptUpdateMutation.mutate()
+            } else if (dialogTyp === 1) {
+                packageUpdateMutation.mutate()
+            } else if (dialogTyp === 2) {
+                clientUpdateMutation.mutate()
+            } else if (dialogTyp === 3) {
+                groupUpdateMutation.mutate()
+            } else if (dialogTyp === 4) {
+                userUpdateMutation.mutate()
+            }
+
+            setRenderComponent(() => false)
+        }
+    }
 
     const remove = () => {
         if (selectedId) {
             if (dialogTyp === 0) {
-                removeScript(selectedId)
-            } else {
-                removePackage(selectedId)
+                scriptRemoveMutation.mutate(selectedId)
+            } else if (dialogTyp === 1) {
+                packageRemoveMutation.mutate(selectedId)
+            } else if (dialogTyp === 2) {
+                clientRemoveMutation.mutate(selectedId)
+            } else if (dialogTyp === 3) {
+                groupRemoveMutation.mutate(selectedId)
+            } else if (dialogTyp === 4) {
+                userRemoveMutation.mutate(selectedId)
             }
 
             setRenderComponent(() => false)
@@ -63,28 +142,35 @@ const DialogManager = ({
     }
 
     return (
-        (renderComponent) ? ReactDOM.createPortal(
-            <Dialog dialogType={dialogTyp} title={t(title)}
-                    unmountComponent={setRenderComponent}
-                    body={dialogContent}
-                    footer={
-                        editMode ?
-                            <>
-                                <DashedOutlinedDialogButton placeholder='Delete' onOnClick={remove}/>
+        <>
+            {
+                (renderComponent) ? ReactDOM.createPortal(
+                    <Dialog title={t(title)}
+                            unmountComponent={setRenderComponent}
+                            body={dialogContent}
+                            footer={
+                                editMode ?
+                                    <>
+                                        {
+                                            userPermitted ? <DashedOutlinedDialogButton placeholder='Remove'
+                                                                                        onOnClick={remove}/> : null
+                                        }
 
-                                <div style={{display: 'flex', gap: '20px'}}>
-                                    {dialogTyp !== 1 ? <SolidOutlinedDialogButton placeholder='Execute'
-                                                                                  onOnClick={() => setRenderComponent(() => false)}/> : null}
-                                    <SolidDialogButton placeholder='Save'/>
-                                </div>
-                            </> :
-                            <>
-                                <span className='fs-tr-body-1'>Version 0.4.5</span>
-                                <br/>
-                                <SolidDialogButton placeholder='Save'/>
-                            </>
-                    }/>,
-            document.querySelector('#layout-container')!) : null
+                                        <div style={{display: 'flex', gap: '20px'}}>
+                                            {
+                                                userPermitted ?
+                                                    <SolidDialogButton placeholder='Save' onOnClick={update}/> : null
+                                            }
+                                        </div>
+                                    </> :
+                                    userPermitted ?
+                                        <div style={{marginLeft: 'auto'}}>
+                                            <SolidDialogButton placeholder={editMode ? 'Save' : 'Add'}/>
+                                        </div> : null
+                            } dialogTyp={dialogTyp}/>,
+                    document.querySelector('#layout-container')!) : null
+            }
+        </>
     )
 }
 
