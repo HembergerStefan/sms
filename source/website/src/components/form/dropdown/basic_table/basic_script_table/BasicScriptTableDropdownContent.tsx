5abfd974baedcd7bbbcaed063d2f643ff80a39cf
ItIsJul1an <64907635+ItIsJul1an@users.noreply.github.com>
2023-03-11 09:01:19 +0100

    code cleanup migrate for react query mutations add check if user is permitted to perform this kind of interaction

--- source/website/src/components/form/dropdown/basic_table/basic_script_table/BasicScriptTableDropdownContent.tsx
+++ source/website/src/components/form/dropdown/basic_table/basic_script_table/BasicScriptTableDropdownContent.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 import {useTranslation} from 'react-i18next'
 
@@ -6,7 +6,8 @@ import NoteAddRoundedIcon from '@mui/icons-material/NoteAddRounded'
 import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded'
 
 import {DataTypes} from '../../../../../data/data_types'
-import useScriptStore from '../../../../../stores/scriptInformationStore'
+import {useRemoveScriptMutation, useUserPermittedQuery} from '../../../../../utils/api/ApiService'
+import useScriptStore from '../../../../../stores/script/scriptInformationStore'
 import useDataListScriptStore from '../../../../../stores/dataListScriptStore'
 
 import Dropdown from '../../Dropdown'
@@ -32,11 +33,22 @@ const BasicScriptTableDropdownContent = ({setMountDropdown}: BasicScriptTableDro
         selectionScriptRows,
     } = useDataListScriptStore()
 
-    const {removeScript} = useScriptStore()
+    const {scripts} = useScriptStore()
 
     const [renderDialogComponent, setRenderDialogComponent] = useState<boolean>(false)
+    const [userPermitted, setUserPermitted] = useState<boolean>(false)
 
+    const userPermittedQuery = useUserPermittedQuery()
+
+
+    const mutation = useRemoveScriptMutation()
     const PAGE_SIZE_ITEMS: number[] = [5, 10, 20, 30, 40, 50]
+
+    useEffect(() => {
+        if (userPermittedQuery.isSuccess && userPermittedQuery.data.name === 'Admin') {
+            setUserPermitted(() => true)
+        }
+    }, [userPermittedQuery.data])
 
     /* User selected item, so change the table size to it */
     const handleChange = (cr: number): void => {
@@ -52,8 +64,10 @@ const BasicScriptTableDropdownContent = ({setMountDropdown}: BasicScriptTableDro
     /* Remove all selected script entries */
     const remove = () => {
         selectionScriptRows.forEach(id => {
-            removeScript(id)
+            mutation.mutate(id)
         })
+
+        setMountDropdown(() => false)
     }
 
     return (
@@ -82,30 +96,36 @@ const BasicScriptTableDropdownContent = ({setMountDropdown}: BasicScriptTableDro
                               handleChange={handleChange}/>
                 </div>
 
-                <hr/>
+                {
+                    userPermitted ?
+                        <>
+                            <hr/>
 
-                <li>
-                    <button onClick={() => add()}>
-                        <div style={{color: 'var(--sc-clr)'}}>
-                            <NoteAddRoundedIcon/>
-                            <span>{t('Add')}</span>
-                        </div>
-                    </button>
-                </li>
+                            <li>
+                                <button onClick={() => add()}>
+                                    <div style={{color: 'var(--sc-clr)'}}>
+                                        <NoteAddRoundedIcon/>
+                                        <span>{t('Add')}</span>
+                                    </div>
+                                </button>
+                            </li>
 
-                <li>
-                    <button onClick={() => remove()}
-                            disabled={selectionScriptRows.length === 0}>
-                        <div style={{color: 'var(--ac-clr-2)'}}>
-                            <DeleteRoundedIcon/>
-                            <span
-                                style={{color: 'var(--ac-clr-2)'}}>{t('Remove')} {selectionScriptRows.length !== 0 ? `(${selectionScriptRows.length})` : ''}</span>
-                        </div>
-                    </button>
-                </li>
+                            <li>
+                                <button onClick={() => remove()}
+                                        disabled={selectionScriptRows.length === 0}>
+                                    <div style={{color: 'var(--ac-clr-2)'}}>
+                                        <DeleteRoundedIcon/>
+                                        <span
+                                            style={{color: 'var(--ac-clr-2)'}}>{t('Remove')} {selectionScriptRows.length !== 0 ? `(${selectionScriptRows.length})` : ''}</span>
+                                    </div>
+                                </button>
+                            </li>
+                        </> : null
+                }
             </ul>
 
             <DialogManager dialogTyp={DataTypes.SCRIPT} title='Add Script' editMode={false}
+                           displayId={scripts.length + 1}
                            renderComponent={renderDialogComponent}
                            setRenderComponent={setRenderDialogComponent}/>
         </>
